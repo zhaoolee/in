b7d1046c54e866834242bfa17f30f52d2ba0ef95
zhaoolee <zhaoolee@gmail.com>
2021-05-05 00:05:24 +0800

    ok

--- pages/About/About.jsx
+++ pages/About/About.jsx
@@ -5,6 +5,7 @@ import Header from '../../components/Header/index.js';
 function About({ Component, pageProps }) {
     return (<div className="about">
         <Header />
+        
         <span>About关于内容</span>
     </div>)
 }
